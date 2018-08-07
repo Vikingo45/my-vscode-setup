e67f2aff9d62804009350b65d8d06e5fa1f1fc47
Victor Alvarez Pajaro <vicalpa@protonmail.com>
2018-08-06 21:10:49 -0400

    update script

--- scripts/install-extensions.sh
+++ scripts/install-extensions.sh
@@ -4,10 +4,12 @@
 
 
 code --install-extension ms-vscode.cpptools
+code --install-extension vincaslt.highlight-matching-tag
 code --install-extension oderwat.indent-rainbow
-code --install-extension monokai.theme-monokai-pro-vscode
+code --install-extension ritwickdey.LiveServer
 code --install-extension be5invis.vscode-icontheme-nomo-dark
 code --install-extension ms-python.python
 code --install-extension liximomo.sftp
+code --install-extension wayou.vscode-todo-highlight
 code --install-extension shardulm94.trailing-spaces
 code --install-extension vscodevim.vim
